f4c0a3ecd7e8917756bf7187788a8e218d1aae1c
haroya <harrisonyang1@gmail.com>
2025-10-14 15:13:51 -0700

    testing

--- main/picklemytickle.spec.ts
+++ main/picklemytickle.spec.ts
@@ -9,21 +9,21 @@ dotenv.config();  //only needed for local dev
 const path = process.env.PATH_MODE ?? 'pj'; //default to pat if path empty
 
 //Set all variables depending on path
-const pstDay : string = functions.getPstDay();
-let username : string;
-let password : string;
-let courtHierarchy : string[];
-let desiredTimes : string[];
-let secondary : string;
+const pstDay: string = functions.getPstDay();
+let username: string;
+let password: string;
+let courtHierarchy: string[];
+let desiredTimes: string[];
+let secondary: string;
 //set creds depending on day
 if (path == 'pj') {
   //creds
   username = process.env.MY_USERNAME as string;
   password = process.env.PASSWORD as string;
   //court hierarachy
-  courtHierarchy = ['8','9','2','1','3','7','5','4','6','10']
+  courtHierarchy = ['8', '9', '2', '1', '3', '7', '5', '4', '6', '10']
   //desired times
-  desiredTimes = ['7:30-8pm','8-8:30pm','8:30-9pm','9-9:30pm',]
+  desiredTimes = ['7:30-8pm', '8-8:30pm', '8:30-9pm', '9-9:30pm',]
   //secondary player
   secondary = 'Tom Tran'
 } else if (path == 'cj') {
@@ -32,26 +32,26 @@ if (path == 'pj') {
   password = process.env.PASSWORD2 as string;
   //court hierarachy
   if (pstDay == 'Wed') {
-    courtHierarchy = ['6','2','8','3','7','5','4','9','1','10']
+    courtHierarchy = ['6', '2', '8', '3', '7', '5', '4', '9', '1', '10']
   } else {
-    courtHierarchy = ['3','8','1','2','5','4','9','6','7','10']
+    courtHierarchy = ['3', '8', '1', '2', '5', '4', '9', '6', '7', '10']
   }
   //desired times
-  desiredTimes = ['7:30-8pm','8-8:30pm','8:30-9pm','9-9:30pm',]
+  desiredTimes = ['7:30-8pm', '8-8:30pm', '8:30-9pm', '9-9:30pm',]
   //secondary player
   secondary = 'Patrick Jung'
 } else if (path == 'el') {
   //creds
   username = process.env.MY_USERNAME3 as string;
   password = process.env.PASSWORD3 as string;
-  
+
   //desired times
   if (pstDay == 'Sat' || pstDay == 'Sun') {
-    desiredTimes = ['8-8:30pm','8:30-9pm','9-9:30pm','9:30-10pm']
-    courtHierarchy = ['3','5','1','4','9','6','2','8','10','7']
+    desiredTimes = ['8-8:30pm', '8:30-9pm', '9-9:30pm', '9:30-10pm']
+    courtHierarchy = ['3', '5', '1', '4', '9', '6', '2', '8', '10', '7']
   } else {
-    desiredTimes = ['5:30-6pm','6-6:30pm','6:30-7pm','7-7:30pm']
-    courtHierarchy = ['9','7','6','1','4','3','2','8','10','5']
+    desiredTimes = ['5:30-6pm', '6-6:30pm', '6:30-7pm', '7-7:30pm']
+    courtHierarchy = ['9', '7', '6', '1', '4', '3', '2', '8', '10', '5']
   }
   //secondary player
   secondary = 'Jimmy Le'
@@ -60,9 +60,9 @@ if (path == 'pj') {
   username = process.env.MY_USERNAME4 as string;
   password = process.env.PASSWORD4 as string;
   //court hierarachy
-  courtHierarchy = ['3','2','10','1','4','6','8','9','7','5']
+  courtHierarchy = ['3', '2', '10', '1', '4', '6', '8', '9', '7', '5']
   //desired times
-  desiredTimes = ['8-8:30pm','8:30-9pm','9-9:30pm','9:30-10pm']
+  desiredTimes = ['8-8:30pm', '8:30-9pm', '9-9:30pm', '9:30-10pm']
   //secondary player
   secondary = 'sherry yi'
 }
@@ -92,14 +92,14 @@ test('bot', async ({ page }) => {
 
   //check for popup
   try {
-    await page.locator(selectors.popup).click({timeout: 2500})
+    await page.locator(selectors.popup).click({ timeout: 2500 })
     console.log('popup existed and closed');
-  } catch (e) {}
+  } catch (e) { }
 
   //select facility (cerritos)
   try {
-    await page.locator(selectors.iPickleCerritosButton).click({timeout: 2500})
-  } catch (e) {}
+    await page.locator(selectors.iPickleCerritosButton).click({ timeout: 2500 })
+  } catch (e) { }
   await page.locator(selectors.bookNow).click()
 
   //select next week
@@ -125,17 +125,17 @@ test('bot', async ({ page }) => {
       const hr = await page.$eval(selectors.hr, el => el.textContent)
       const min = await page.$eval(selectors.min, el => el.textContent)
       const sec = await page.$eval(selectors.sec, el => el.textContent)
-      if (loopCounter%10 == 0) {
+      if (loopCounter % 10 == 0) {
         console.log(`time left remaining: ${hr}:${min}:${sec}`)
       }
-    } catch {}
+    } catch { }
     loopCounter++
   }
 
   await page.evaluate(() => {
     window.scrollBy(0, window.innerHeight / 2);
   });
-  
+
   //on wednesday, wait 1 sec except for jc
   if (pstDay == 'Wed' && path != 'jc') {
     console.log("Waiting 1 sec for JC to book first.")
@@ -143,13 +143,13 @@ test('bot', async ({ page }) => {
   }
 
   //select times
-  let selected : boolean = false
+  let selected: boolean = false
   for (const time of desiredTimes) {
     const locator = page.locator(functions.desiredTimePath(time));
     try {
       await locator.waitFor({ timeout: 100 });
       console.log(`${time} selected`);
-      
+
       //select time
       await page.locator(functions.desiredTimePath(time)).click()
       selected = true;
@@ -163,13 +163,13 @@ test('bot', async ({ page }) => {
   }
   await page.waitForTimeout(100);
 
-  let selectedCourts : string[] = []
+  let selectedCourts: string[] = []
   //select earliest court
   for (const court of courtHierarchy) {
-      //add selected court to other array
-      selectedCourts.push(court)
+    //add selected court to other array
+    selectedCourts.push(court)
     try {
-      await page.locator(functions.courtPath(court)).click({timeout: 100})
+      await page.locator(functions.courtPath(court)).click({ timeout: 100 })
       console.log(`Court ${court} selected`)
       break;
     } catch {
@@ -183,15 +183,15 @@ test('bot', async ({ page }) => {
     //don't want the bot doing anything
   } else {
     //Next
-    await page.locator(selectors.nextButton).click({timeout: 5000})
+    await page.locator(selectors.nextButton).click({ timeout: 5000 })
 
     //select number of users
-    await page.locator(selectors.twoPlayers).click({timeout:1000})
-    await page.locator(selectors.addUsers).click({timeout:1000})
+    await page.locator(selectors.twoPlayers).click({ timeout: 1000 })
+    await page.locator(selectors.addUsers).click({ timeout: 1000 })
 
     //search users
-    await page.locator(functions.playerPath(secondary)).click({timeout:1000})
-    await page.locator(selectors.userSelectionNext).click({timeout:1000})
+    await page.locator(functions.playerPath(secondary)).click({ timeout: 1000 })
+    await page.locator(selectors.userSelectionNext).click({ timeout: 1000 })
 
     //listen for alert
     let alertAppeared = false;
@@ -203,7 +203,7 @@ test('bot', async ({ page }) => {
     });
 
     //BOOK
-    await page.locator(selectors.bookButton).click({timeout:1000})
+    await page.locator(selectors.bookButton).click({ timeout: 1000 })
     await page.waitForTimeout(1000)
 
     console.log(alertAppeared ? '❌ Alert appeared' : '✅ No alert appeared');
@@ -213,8 +213,8 @@ test('bot', async ({ page }) => {
       let confirmationCount = await page.locator(selectors.confirmationNumber).count()
       while (confirmationCount < 1 && selectedCourts.length < courtHierarchy.length) { //if booking confirmation is not found
         //go back to court selection
-        try{
-          await page.locator(selectors.selectDateTime).click({timeout:5000})
+        try {
+          await page.locator(selectors.selectDateTime).click({ timeout: 5000 })
         } catch {
           break;  //if it errors out here, that means reservation was successful, the page just didn't load in time
         }
@@ -226,7 +226,7 @@ test('bot', async ({ page }) => {
           //add selected court to other array
           selectedCourts.push(court)
           try {
-            await page.locator(functions.courtPath(court)).click({timeout: 100})
+            await page.locator(functions.courtPath(court)).click({ timeout: 100 })
             console.log(`Court ${court} selected`)
             break;
           } catch {
@@ -234,9 +234,9 @@ test('bot', async ({ page }) => {
           }
         }
         //go back to book
-        await page.locator(selectors.checkout).click({timeout:5000})
+        await page.locator(selectors.checkout).click({ timeout: 5000 })
         //BOOK
-        await page.locator(selectors.bookButton).click({timeout:5000})
+        await page.locator(selectors.bookButton).click({ timeout: 5000 })
         await page.waitForTimeout(1000)
         confirmationCount = await page.locator(selectors.confirmationNumber).count()
       }
